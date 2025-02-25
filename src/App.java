4fc6d09ffb185bce3422303628af3d54fa43231b
Juan Gallego <juanesteban.gallegol.02@gmail.com>
2025-02-24 22:39:07 -0500

    add metodos

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
 
 public class App {
 	
-	public static double sumar(double valorA, double valorB) {
+	public static void main(String[] args) {
+			
+	}
+	
+	public double sumar(double valorA, double valorB) {
 		return valorA + valorB;
 	}
 	
-	public static double restar(double valorA, double valorB) {
+	public double restar(double valorA, double valorB) {
 		return valorA - valorB;
 	}
 	
